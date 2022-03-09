5120fa57f30b6faf31099b576ba711f4245e3f6f
Cristian Cezar Moisés <86272521+cristiancmoises@users.noreply.github.com>
2022-03-09 10:12:06 -0300

    Update chocolate.bat

--- chocolate.bat
+++ chocolate.bat
@@ -1,54 +1,99 @@
-echo off
+echo OFF
+
 NET SESSION >nul 2>&1
-IF %ERRORLEVEL EQU 0 (
-  echo.
+
+IF %ERRORLEVEL% EQU 0 (
+
+   echo.
+
 ) ELSE (
-  echo. ---------------------------------------------------------------
-  echo ERROR: YOU ARE NOT RUNNING THIS WITH ADMINISTRATOR PRIVILEGES
-  echo. ---------------------------------------------------------------
-  echo. If you're seeing this, it means you don't have admin privileges"
-  pause
-  echo.
-  echo. You will need to restart this program with Administrator privileges by right-clicking
-  and select "Run As Administrator"
-  
-  pause
-  
-  echo.
-  echo Press any key to leave this program. Make sure to Run As Administrator next time!
-  
-  pause
-  
-  EXIT /B 1
-  
+
+   echo.-------------------------------------------------------------
+
+   echo ERROR: YOU ARE NOT RUNNING THIS WITH ADMINISTRATOR PRIVILEGES.
+
+   echo. -------------------------------------------------------------
+
+   echo. If you're seeing this, it means you don't have admin privileges!
+
+   pause
+
+   echo.
+
+   echo. You will need to restart this program with Administrator privileges by right-clicking and select "Run As Administrator"
+
+   pause 
+
+    echo.
+
+   echo Press any key to leave this program. Make sure to Run As Administrator next time!
+
+   pause
+
+   EXIT /B 1
+
 )
 
-powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex
-((New-Object System.Net.WebClient).DownloadString('htttps://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco feature enable -n=allowGlobalConfirmation
 
 echo Chocolatey is ready to begin installing packages!
+
 pause
 
 @rem ----[ Whatever you want to install, place it below this point, each item on its own line (to make it easier to find later on. ] ----
 
-choco install flashplayerplugin
-choco install chocolatey-core.extension
-choco install flashplayeractivex
+choco install winrar
 choco install 7zip.install
-choco install curl
+choco install firefox
 choco install dotnetfx
-choco install ffmpeg
+choco install flashplayerplugin
 choco install vlc
-choco install adobe air
+choco install chocolatey-core.extension
+choco install anydesk.install
+choco install chocolatey-dotnetfx.extension
+choco install dotnet4.7
+choco install dotnet4.7.1
+choco install dotnet4.6
+choco install dotnet4.6.2
+choco install dotnet4.6.1
+choco install dotnet4.5
+choco install dotnet4.0
+choco install dotnet3.5
+choco install chromium
+choco install nextcloud-client
+choco install gimp
+choco install gimp-data-extras
+choco install photogimp
+choco install krita
 choco install everything
+choco install adobereader
+choco install flashplayeractivex
+choco install jre8
+choco install chocolatey-windowsupdate.extension
+choco install notepadplusplus.install
+choco install git.install
+choco install adobeshockwaveplayer
+choco install sumatrapdf
+choco install steam-client
+choco install k-litecodecpackfull
+choco install thunderbird
+choco install keepass.install
+choco install wireshark
+choco install audacity
+choco install obs-studio
+choco install obs-virtualcam
+choco install droidcamclient
+choco install openshot
+choco install librewolf
+choco install tor
+choco install tor-browser
+choco install protonvpn
 
-@rem ----[ You can add as many programs here as you desire!] ----
+@rem ---[You can add as many programs here as you desire!]----
 
 pause
 echo.
 echo Your installation is complete.
 pause
-
-
