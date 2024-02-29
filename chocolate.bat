cb369549ef0f1b2c3f27cccea28a8830fb70ccd2
Cristian Cezar Moisés <86272521+cristiancmoises@users.noreply.github.com>
2024-02-29 12:29:52 +0000

    Update chocolate.bat

--- chocolate.bat
+++ chocolate.bat
@@ -46,8 +46,8 @@ pause
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n=allowGlobalConfirmation
 choco feature enable -n useFipsCompliantChecksums
-Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll 
+powershell Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false
+powershell Install-WindowsUpdate -MicrosoftUpdate -AcceptAll 
 Get-WuInstall -AcceptAll -IgnoreReboot
 choco install winrar
 choco install 7zip.install
